c73dc3d885f21bc33e58f31fc3773a1848b002b3
Ankur Seth <iankurseth@gmail.com>
2022-03-01 12:31:00 +0530

    Printing zipfile contents to the console

--- src/Tadpole/Tadpole.js
+++ src/Tadpole/Tadpole.js
@@ -1,12 +1,32 @@
+import React from 'react';
+
+const JSZip = require('jszip');
 
 
 
 export default function Tadpole () {
+	const fup = React.useRef(null);
+
+	function handleUpload () {
+		const file = fup.current.files[0];
+		if (!file) return;
+
+		const zip = new JSZip();
+		zip.loadAsync(file).then(function (zip) {
+			// console.log(zip);
+			for (let file in zip.files) {
+				console.log(file);
+			}
+		});
+	}
 
 	return (
 		<div className="Tadpole">
-			<header class="min-h-screen bg-green-500 text-white flex">
-				<h2 className="m-auto text-5xl">Tadpole</h2>
+			<header className="min-h-screen bg-green-500 text-white flex flex-col">
+				<h2 className="m-auto text-5xl" onClick={handleUpload}>Tadpole</h2>
+				<div className="bg-green-400 p-4">
+					<input ref={fup} type="file" name="" id="" />
+				</div>
 			</header>
 		</div>
 	);
